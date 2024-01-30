bf1c51af9727e4ef2f21f4f24edd55f73393105a
ramachell <mjh1548@gmail.com>
2024-01-31 00:02:08 +0900

    순서기록

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({ xIsNext, squares, onPlay }) {
+  // Game 로부터 받아오므로 여기선 제거했음
+  // x, o 의 순서 관리
+  // const [xIsNext, setXIsNext] = useState(true);
+  // 각각의 네모의 상태관리
+  // const [squares, setSquares] = useState(Array(9).fill(null));
 
+  // 승자 판독
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -37,8 +41,14 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    // 아래 코드와 동일
+    // nextSquares[i] = xIsNext ? "X" : "O";
+
+    onPlay(nextSquares);
+
+    // game에서 상태관리하므로 여기선 필요없어져서 제거
+    // setSquares(nextSquares);
+    // setXIsNext(!xIsNext);
   }
   return (
     <>
@@ -75,9 +85,59 @@ function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
+
+    // const a = lines[i][0];
+    // const b = lines[i][1];
+    // const c = lines[i][2];
+
+    // i = 0 일때 lines = [0,1,2]
+    // 즉 0,1,2 번칸이 전부 같을때 승자 판독해줌 X, O 중 하나 리턴
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
   return null;
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // ...은 기존 history의 목록을 나열후 nextSquares 를 젤 뒤에 집어넣음
+    setHistory([history, nextSquares]);
+
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    console.log(squares);
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
